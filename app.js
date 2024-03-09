ee19052828c7743d0b675a9e2088d8810e344c9b
unknown <sourabh321joshi@gmail.com>
2024-03-09 11:43:55 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,12 @@
+const parent = React.createElement("div" ,{id:"parent" ,})
+
+
+
+
+
+const heading = React.createElement("h1" ,
+{id:"heading", xyz:"abc"},"hello world from react");
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
