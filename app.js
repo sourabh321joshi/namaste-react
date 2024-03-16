1dcc08f1130cfc2a5313de8e1d19f8808a0ab416
unknown <sourabh321joshi@gmail.com>
2024-03-16 21:37:12 +0530

    episode - 01

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const parent = React.createElement("div" ,{id:"parent" ,})
-
-
-
-
-
-const heading = React.createElement("h1" ,
-{id:"heading", xyz:"abc"},"hello world from react");
-
+const parent = React.createElement("div" ,{id:"parent"},
+React.createElement("div" , {id:"child"} , 
+[React.createElement("h1" , {} , "i am h1 tag"),
+React.createElement("h2" , {} , "i am h2 tag")]
+)
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
